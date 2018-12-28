1572a313fd0c95ddb54294326b96602a332901ef
imanj12 <imanj12@gmail.com>
2018-12-27 23:35:32 -0500

    added delete to cors

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module SkibuddyBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
 
