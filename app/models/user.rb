e3e97a73ee584951d1583fc4a5cfc56596a66404
imanj12 <imanj12@gmail.com>
2019-02-28 16:27:56 -0500

    updated cors policy to only accept from skibuddy-frontend on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
    has_secure_password
    validates :username, presence: true
    validates :username, uniqueness: true, allow_blank: true
-
-   validates :city, presence: true
    
    validates :password, length: {minimum: 6}, allow_blank: true
 
