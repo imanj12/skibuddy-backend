c62d620d566bfd7969e06fd29184cfb14d869e71
imanj12 <imanj12@gmail.com>
2019-02-28 16:28:16 -0500

    updated cors policy to only accept from skibuddy-frontend on heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://nskibuddy-frontend.herokuapp.com'
+    origins 'https://skibuddy-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
